f6169343e2aaa815f2eb7e893e5a4410fa9001eb
lthiery <me@louisthiery.com>
2020-06-13 11:27:43 -0700

    fill full buffer

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,9 @@ use std::sync::{Arc, Mutex, Weak};
 const LEDGER_VID: u16 = 0x2c97;
 const LEDGER_USAGE_PAGE: u16 = 0xFFA0;
 const LEDGER_CHANNEL: u16 = 0x0101;
-const LEDGER_PACKET_SIZE: u8 = 64;
+// for Windows compatability, we prepend the buffer with a 0x00
+// so the actual buffer is 64 bytes
+const LEDGER_PACKET_SIZE: u8 = 65;
 
 const LEDGER_TIMEOUT: i32 = 10_000_000;
 
